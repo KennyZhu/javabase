5474210d3123bb9b74f72840f5c0560adda65e97
kennyzhu <yanlongzhu2010@gmail.com>
2014-10-24 21:31:12 +0800

    add third platform type

---
+++ src/main/java/com/zkenny/core/framework/third/ThirdTypeEnum.java
@@ -0,0 +1,19 @@
+package com.zkenny.core.framework.third;
+
+/**
+ * Desc:第三方登录类型
+ * <p/>Date: 2014/10/24
+ * <br/>Time: 21:27
+ * <br/>User: kennyzhu
+ */
+public enum ThirdTypeEnum {
+
+    QQ("qq"),
+    WeiBo("weibo");
+    private String platform;
+
+    private ThirdTypeEnum(String platform) {
+        this.platform = platform;
+    }
+
+}
